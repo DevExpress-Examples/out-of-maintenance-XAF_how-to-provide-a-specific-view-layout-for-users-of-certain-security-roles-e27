8b10e724a5eab0184d332ec587960283000f04ca
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/E274.Module/Controllers/CustomizeViewAgainstRoleMainWindowController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Security;
+
+namespace E274.Module.Controllers {
+    public class CustomizeViewAgainstRoleMainWindowController : WindowController {
+        private const string AdministratorsViewSuffix = "Administrators";
+        private const string UsersViewSuffix = "Users";
+        public CustomizeViewAgainstRoleMainWindowController() {
+            TargetWindowType = WindowType.Main;
+        }
+        protected override void OnActivated() {
+            base.OnActivated();
+            Application.ViewCreating += Application_ViewCreating;
+        }
+        //Create corresponding views in the application model first! See the Model.DesignedDiffs.XAFML file for an example.
+        void Application_ViewCreating(object sender, ViewCreatingEventArgs e) {
+            string roleBasedViewId = String.Format("{0}_{1}", e.ViewID, ((ISecurityUserWithRoles)SecuritySystem.CurrentUser).IsUserInRole(AdministratorsViewSuffix) ? AdministratorsViewSuffix : UsersViewSuffix);
+            if (Application.FindModelView(roleBasedViewId) != null) {
+                e.ViewID = roleBasedViewId;
+            }
+        }
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+            Application.ViewCreating -= Application_ViewCreating;
+        }
+    }
+}
---
+++ VB/E274.Module/BusinessObjects/Contact.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Persistent.BaseImpl
+Namespace E274.Module.BusinessObjects
+	<DefaultClassOptions>
+	Public Class Contact
+		Inherits Person
+
+		Public Sub New(ByVal session As DevExpress.Xpo.Session)
+			MyBase.New(session)
+
+		End Sub
+	End Class
+End Namespace
