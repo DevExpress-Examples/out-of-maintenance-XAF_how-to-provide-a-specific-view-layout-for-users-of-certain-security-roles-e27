96398801c21250374813490b3a393bf6bf83aa2a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WinWebSolution.Module/ShowViewAgainstRoleViewController.cs
@@ -0,0 +1,38 @@
+using Demo;
+using System;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model;
+
+namespace WinWebSolution.Module {
+    public partial class ShowViewAgainstRoleViewController : ViewController {
+        public const string AdministratorsViewSuffix = "Administrators";
+        public const string UsersViewSuffix = "Users";
+        private XafApplication app;
+        protected override void OnFrameAssigned() {
+            base.OnFrameAssigned();
+            if (app == null) {
+                app = Application;
+                app.ViewShowing += Application_ViewShowing;
+            }
+        }
+        protected override void Dispose(bool disposing) {
+            if (disposing && app != null)
+                app.ViewShowing -= Application_ViewShowing;
+            base.Dispose(disposing);
+        }
+        private void Application_ViewShowing(object sender, ViewShowingEventArgs e) {
+            IModelView modelView = GetRoleModelView(e.View.Id);
+            if (modelView != null)
+                e.View.SetModel(modelView);
+        }
+        //Dennis: create corresponding views in the application model first.
+        private IModelView GetRoleModelView(string defaultViewID) {
+            string customViewId = defaultViewID;
+            if (SecuritySystemHelper.IsUserInRole(AdministratorsViewSuffix))
+                customViewId += String.Format("_{1}", defaultViewID, AdministratorsViewSuffix);
+            else
+                customViewId += String.Format("_{1}", defaultViewID, UsersViewSuffix);
+            return app.FindModelView(customViewId);
+        }
+    }
+}
